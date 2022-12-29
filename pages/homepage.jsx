e56407c7157841f363c480ef1c51c9b384135087
Rafael Cardoso <rscedf@gmail.com>
2022-12-29 10:59:11 -0300

    ajustando colunas

--- pages/homepage.jsx
+++ pages/homepage.jsx
@@ -3,7 +3,9 @@ import ImageIcon from '../src/components/layout/imageIcon'
 import ImageProject from '../src/components/layout/imageProject'
 import MyData from '../src/components/layout/myData'
 import Container from '../src/components/layout/container'
+import ContainerColumn from '../src/components/layout/containerColumn'
 import TextH1 from '../src/components/tipografia/h1'
+import TextH2 from '../src/components/tipografia/h1'
 import TextH3 from '../src/components/tipografia/h3'
 import Paragrafo from '../src/components/tipografia/paragrafo'
 import Icon from '../src/components/icon/icon'
@@ -47,41 +49,38 @@ function HomePage() {
         />
       </ImageIcon>
       <Container>
-        <TextH3>Meus Projetos</TextH3>
-        <TextH3>Meus Dados</TextH3>
+        <TextH2>Meus Projetos</TextH2>
+        <TextH2>Meus Dados</TextH2>
       </Container>
 
       <Container>
         <ImageProject>
-          <Icon
-            iconName="socialDevImage"
-            sizeWhidt="200px"
-            sizeHeight="150px"
-            link="https://social-dev-seven.vercel.app/login"
-          />
-          <Paragrafo>social dev é uma rede social criada duranteo curso</Paragrafo>
-          <Icon
-            iconName="jogoVelha"
-            sizeWhidt="200px"
-            sizeHeight="150px"
-            link="https://react-jogo-da-velha-rho.vercel.app/"
-          />
-          <Paragrafo>jogo da velha foi criado duranteo curso</Paragrafo>
+          <ContainerColumn>
+            <Icon
+              iconName="socialDevImage"
+              sizeWhidt="200px"
+              sizeHeight="150px"
+              link="https://social-dev-seven.vercel.app/login"
+            />
+            <Paragrafo>social dev é uma rede social criada duranteo curso</Paragrafo>
+          </ContainerColumn>
+          <ContainerColumn>
+            <Icon
+              iconName="jogoVelha"
+              sizeWhidt="200px"
+              sizeHeight="150px"
+              link="https://react-jogo-da-velha-rho.vercel.app/"
+            />
+            <Paragrafo>jogo da velha foi criado duranteo curso</Paragrafo>
+          </ContainerColumn>
         </ImageProject>
 
         <MyData>
-          <Icon
-            iconName="socialDevImage"
-            sizeWhidt="200px"
-            sizeHeight="150px"
-            link="https://social-dev-seven.vercel.app/login"
-          />
-          <Icon
-            iconName="jogoVelha"
-            sizeWhidt="200px"
-            sizeHeight="150px"
-            link="https://react-jogo-da-velha-rho.vercel.app/"
-          />
+          <ContainerColumn>
+            <TextH3>Nome: Rafael Silva Cardoso</TextH3>
+            <TextH3>Nome: Rafael Silva Cardoso</TextH3>
+            <TextH3>Nome: Rafael Silva Cardoso</TextH3>
+          </ContainerColumn>
         </MyData>
       </Container>
     </>
